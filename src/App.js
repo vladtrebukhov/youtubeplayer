2eda526f80d6ee63f5d80f754fdd702f6255b86d
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-09-23 13:27:13 -0400

    added search function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SearchBar from "./components/searchbar.jsx";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleSearch = event => {
+    let value = event.target.value;
+    console.log(value);
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="main-player">
+        <SearchBar handleSearch={this.handleSearch} />
       </div>
     );
   }
