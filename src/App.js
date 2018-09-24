234a83183c6df3a88752131cee7211ee0caf5794
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-09-24 13:07:05 -0400

    added media player, youtube search packages to call API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from "react";
 import SearchBar from "./components/searchbar.jsx";
 import "./App.css";
+import MediaPlayer from "./components/mediaplayer";
+import Search from "youtube-search";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      videoUrl: null
+    };
+  }
+
+  componentDidMount() {
+    var opts = {
+      maxResults: 20,
+      key: process.env.REACT_APP_API_KEY
+    };
+
+    Search("jsconf", opts, function(err, results) {
+      if (err) return console.log(err);
+
+      let randomVideo = results[Math.floor(Math.random() * results.length)];
+    });
+
+    // this.setState({
+    //   videoURL: this.randomVideo
+    // });
   }
 
   handleSearch = event => {
+    event.preventDefault();
     let value = event.target.value;
     console.log(value);
   };
 
   render() {
     return (
-      <div className="main-player">
+      <div className="main-container">
         <SearchBar handleSearch={this.handleSearch} />
+        <div className="media">
+          <MediaPlayer />
+        </div>
       </div>
     );
   }
