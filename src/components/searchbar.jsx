0359fd700aa6c0705f9a08867fb417463ca6dca0
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-09-25 19:37:39 -0400

    bug fix

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  handleSearch = event => {
-    let value = event.target.value;
-    console.log(value);
-  };
-
   render() {
     return (
-      <form onChange={this.props.handleSearch} className="search-form">
+      <form onSubmit={this.props.handleSearch} className="search-form">
         <input type="text" placeholder="search for music or videos" />
       </form>
     );
