5a3080eb790087241cde92a75fc4636944c6759c
Vlad Trebukhov <vladtrebukhov123@gmail.com>
2018-10-04 00:23:27 -0400

    added intermediate function to pass user input

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  createSearch = event => {
+    //we need this intermediate state to pass in the user input to the handleSearch function in our main app component
+    event.preventDefault();
+    const userInput = {
+      input: this.userSearch.value
+    };
+
+    this.props.handleSearch(userInput);
+    this.inputForm.reset();
+  };
+
   render() {
     return (
-      <form onSubmit={this.props.handleSearch} className="search-form">
-        <input type="text" placeholder="search for music or videos" />
+      <form
+        ref={input => (this.inputForm = input)}
+        //cannot directly call a function within
+        onSubmit={this.createSearch}
+        className="search-form"
+      >
+        <input
+          ref={input => (this.userSearch = input)}
+          type="text"
+          placeholder="search for music or videos"
+        />
       </form>
     );
   }
